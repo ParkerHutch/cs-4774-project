09c46babc1de73152c15d045fe72cfb7b3fc3068
Parker Charles Hutchinson <parker.hutch@outlook.com>
2023-04-26 10:43:25 -0400

    added pokemon and region information

--- trainer_profile.php
+++ trainer_profile.php
@@ -1,9 +1,6 @@
 
 <html>
    <h1>Trainer Profile Page</h1>
-   <form action="trainer_profile.php" method="post">
-      Name: <input type="text" name="animal_name"><br>
-   <input type="submit" />
 </html>
 <?php
 
@@ -13,10 +10,11 @@ $host = 'cs-4750-project-381321:us-east4:cs-4750-project';
 $dbname = 'Pokemon';
 $dsn = "mysql:unix_socket=/cloudsql/cs-4750-project-381321:us-east4:cs-4750-project;dbname=Pokemon";
 
-// TODO replace '2' in each line below with the logged-in trainer's ID in the future
-$get_trainer_sql = "SELECT trainerID, name, friendGroup FROM trainer WHERE trainerID = 2"; 
-$get_trainer_region_sql = "SELECT name FROM region WHERE regionID = (SELECT regionID FROM is_from WHERE trainerID = 2)";
-$get_trainer_pokemon_sql = "SELECT * FROM pokemon WHERE pokemon.team = (SELECT number FROM leads WHERE trainerID = 2)";
+$loggedInTrainerId = 2; // TODO replace '2' with the logged-in trainer's ID in the future
+$get_trainer_sql = "SELECT trainerID, name, friendGroup FROM trainer WHERE trainerID = $loggedInTrainerId"; 
+$get_trainer_region_sql = "SELECT name FROM region WHERE regionID = (SELECT regionID FROM is_from WHERE trainerID = $loggedInTrainerId)";
+$get_trainer_pokemon_sql = "SELECT Name, Type1, Type2 FROM pokemon WHERE pokemon.team = (SELECT number FROM leads WHERE trainerID = $loggedInTrainerId)";
+$get_trainer_friend_group = "SELECT group_name FROM trainerFriendGroup WHERE trainerID = $loggedInTrainerId";
 
 try
 {
@@ -32,9 +30,35 @@ try
    
    foreach ($db->query($get_trainer_region_sql) as $row) {
 
-      echo "<p>Your region is {$row[name]}</p>";
+      echo "<p>Your region: <strong>{$row[name]}</strong></p>";
    }
-   
+
+   foreach ($db->query($get_trainer_friend_group) as $row) {
+
+      echo "<p>Your friend group: <strong>{$row[group_name]}</strong></p>";
+   }
+
+   echo "<h1>Your Pokemon</h1>";
+   echo "<table>
+            <thead>
+               <tr>
+                  <th>Name</th>
+                  <th>Type1</th>
+                  <th>Type2</th>
+               </tr>
+               </thead>
+               <tbody>";
+      
+   foreach ($db->query($get_trainer_pokemon_sql) as $row) {
+      echo "<tr>";
+      echo "<td>{$row[Name]}</td>";
+      echo "<td>{$row[Type1]}</td>";
+      echo "<td>{$row[Type2]}</td>";
+      echo "</tr>";
+   }
+   echo "</tbody>";
+   echo "</table>";
+
 }
 catch (PDOException $e)
 {
